bc0940aa4cd745fb06b7d657afd010159f13e50d
Kilian Drayne <kiliandrayne@hotmail.com>
2017-11-10 02:07:34 +0000

    styling

---
+++ index.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+	$(".title").click(function() {
+  		$('#myModal').modal('show');
+	});
+
+	$(".img-preview").click(function() {
+  		$('#myModal').modal('show');
+	});
+});
