eaeb4d7086293947f2e319ca8c7897a3699c96fc
Kilian Drayne <kiliandrayne@hotmail.com>
2017-11-10 03:00:26 +0000

    added tabs

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ $( document ).ready(function() {
 	});
 
 	$(".img-preview").click(function() {
-  		$('#myModal').modal('show');
+  		$('#myModal').modal('toggle');
 	});
+
+
+	//---------tabs ----------------------------
+ $('ul.navbar-nav li').click(function(){
+    var tab_id = $(this).attr('data-tab');
+
+    $('ul.tabs li').removeClass('current');
+    $('.tab-content').hide();
+
+    $(this).addClass('current');
+    $("#"+tab_id).show();
+  })
+
 });
+
+  function populateNews(category){
+  	alert("test");
+  }
