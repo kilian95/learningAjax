4d326cabfe35c9a38a5e88148d21c718ba35e14c
Kilian Drayne <kiliandrayne@hotmail.com>
2017-11-12 02:45:09 +0000

    home page now displaying latest news from al jazeera and popular news from bbc

--- index.js
+++ index.js
@@ -30,23 +30,68 @@ $( document ).ready(function() {
     $("#"+tab_id).show();
   })
 
-  //-------------ajax---------------------------------
+  //-------------ajax gaurdian---------------------------------
+ 
+  // $.ajax( 
+  // {
+  //     type: "GET",
+  //     dataType: "jsonp",
+  //     cache: false,
+  //     url: "http://content.guardianapis.com/search?section=world&api-key=a4f92163-c44e-49ca-b122-c72594c02b28",
+  //     success: function(data) 
+  //     {
+        
+  //         $("#mainTitle").append("<a href='#''>" + data.response.results[0].webTitle + "</a>");
+          
+  //       for (var i = 0; i < 4; i++)
+  //       {
+  //         $("#title" + i).append("<a href='#''>" + data.response.results[i].webTitle + "</a>");
+  //       }
+        
+  //     }
+  // })
+
+
+
+  //------------newsapi----------------------
+
   $.ajax( 
   {
       type: "GET",
-      dataType: "jsonp",
+      dataType: "json",
       cache: false,
-      url: "http://content.guardianapis.com/search?section=world&api-key=a4f92163-c44e-49ca-b122-c72594c02b28",
+      url: "https://newsapi.org/v1/articles?source=al-jazeera-english&sortBy=latest&apiKey=37666d41fc4e49f9acc12919662f769d",
       success: function(data) 
       {
+        $("#mainTitle").append("<a href='#''>" + data.articles[0].title + "</a>");
+        $("#mainDesc").append(data.articles[0].description);
+        $("#mainImg").append("<img class='img-preview' src='"+ data.articles[0].urlToImage + "' width='100%''>");
         
-          $("#mainTitle").append("<a href='#''>" + data.response.results[0].webTitle + "</a>");
-          
         for (var i = 0; i < 4; i++)
         {
-          $("#title" + i).append("<a href='#''>" + data.response.results[i].webTitle + "</a>");
+          $("#title" + i).append("<a href='#''>" + data.articles[i].title + "</a>");
+          $("#desc" + i).append(data.articles[i].description);
+          $("#article" + i).prepend("<img class='img-preview' src='"+ data.articles[i].urlToImage + "' width='100%''>");
         }
-        
+      }
+  })
+
+  $.ajax( 
+  {
+      type: "GET",
+      dataType: "json",
+      cache: false,
+      url: "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=37666d41fc4e49f9acc12919662f769d",
+      success: function(data) 
+      {
+       
+        for (var i = 0; i < 2; i++)
+        {
+          $("#popularTitle" + i).append("<a href='#''>" + data.articles[i].title + "</a>");
+          $("#popularDesc" + i).append(data.articles[i].description);
+          $("#popular" + i).prepend("<img class='img-preview' src='"+ data.articles[i].urlToImage + "' width='100%''>");
+          
+        }
       }
   })
 });
@@ -55,3 +100,7 @@ $( document ).ready(function() {
   function populate(){
   	$('body').load('test.html');
   }
+
+  //when tab changes:
+  //1. Populate tab with template
+  //2. Call function to populate tab with news articles using ajax.
